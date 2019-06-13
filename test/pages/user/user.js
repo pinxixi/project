6b4bc5e08f7d9eebd3bff5170d31c034669808b2
233zhao <2926093155@qq.com>
2019-06-13 09:11:57 +0800

    完善个人中心

--- test/pages/user/user.js
+++ test/pages/user/user.js
@@ -5,16 +5,95 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    map: null,
+    mapData:{},
+    data:{
+      controls: [{
+        id: 1,
+        iconPath: '/pages/image/icon4.gif',
+        position: {
+          left: 0,
+          top: 300 - 50,
+          width: 35,
+          height: 30
+        },
+        clickable: true
+      }],
+      markers:[]
+    },
+    newMarker: {
+      iconPath: "/pages/image/icon8.gif",
+      id: 0,
+      latitude: 0,
+      longitude: 0,
+      width: 20,
+      height: 15
+    },
+    listData:{
+      list1:[
+        {
+          name: "待付款",
+          icon: "/pages/image/icon1.gif",
+          num: "3"
+        }, {
+          name: "待分享",
+          icon: "/pages/image/icon2.gif",
+          num: "0"
+        }, {
+          name: "待发货",
+          icon: "/pages/image/icon3.gif",
+          num: "0"
+        }, {
+          name: "待收货",
+          icon: "/pages/image/icon4.gif",
+          num: "0"
+        }, {
+          name: "待评价",
+          icon: "/pages/image/icon5.gif",
+          num: "0"
+        }
+      ]
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.data.map = wx.createMapContext("map");
+    this.setData({
+      map: this.data.map
+    })
   },
-
+  getuserinfo(e){
+    console.log(e)
+  },
+  getLocation(){
+    wx.getLocation({
+      type: 'wgs84',
+      success: res=> {
+        // console.log(res)
+        let { latitude, longitude} = res;
+        // console.log(latitude,longitude)
+        this.data.mapData={latitude,longitude}
+        this.setData({
+          mapData: this.data.mapData
+        })
+        let { map,data,newMarker } = this.data;
+        console.log(map)
+        newMarker.id= 0
+        newMarker.latitude = latitude
+        newMarker.longitude = longitude
+        data.markers.push(newMarker)
+        this.setData({
+          data
+        })
+        console.log(this.data.data)
+      }
+      
+    })
+    
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
