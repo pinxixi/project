03c2c47098c2b32e8503aeee8bec3816666cdf31
possess201314 <1948475178@qq.com>
2019-06-20 08:41:53 +0800

    解决冲突

--- test/pages/index/index.js
+++ test/pages/index/index.js
@@ -21,13 +21,10 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-<<<<<<< HEAD
       wx.navigateTo({
         url: '/pages/home/home',
       })
       console.log(1)
-=======
->>>>>>> f7982ab2e5ecf2a7a12a93aa0db28e6558e0408e
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -50,7 +47,6 @@ Page({
       })
     }
   },
-<<<<<<< HEAD
   onShow(){
     setTimeout(()=>{                      
       this.setData({
@@ -61,8 +57,6 @@ Page({
       })
     },2000)
   },
-=======
->>>>>>> f7982ab2e5ecf2a7a12a93aa0db28e6558e0408e
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
