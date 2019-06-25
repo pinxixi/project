4045806a338261629ec3bbf86741b5d054279398
233zhao <2926093155@qq.com>
2019-06-25 09:47:37 +0800

    同步进度

--- test/pages/ticket/ticket.js
+++ test/pages/ticket/ticket.js
@@ -4,7 +4,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isShow: false
+    isShow: false,
+    list:[
+      {
+        modul: "modul1",
+        title: "收藏+关注",
+        money: 5
+      },
+      {
+        modul: "modul2",
+        title: "满199立减",
+        money: 20
+      }
+    ],
+    cIndex: 0
   },
   /**
    * 生命周期函数--监听页面加载
@@ -12,12 +25,30 @@ Page({
   onLoad: function (options) {
 
   },
-  showPopup(){
+  show(){
     let {isShow} = this.data;
     isShow=!isShow;
-    console.log(isShow)
     this.setData({
       isShow
     })
+  },
+  use(e){
+    this.show()
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      cIndex: index
+    })
+  },
+  confirm(){
+    this.data.list.splice(this.data.cIndex,1)
+    this.setData({
+      list: this.data.list
+    })
+  },
+  cancel(){
+    // console.log("取消")
+  },
+  onClose(){
+    this.show()
   }
 })
